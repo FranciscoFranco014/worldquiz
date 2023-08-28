b32559245d0fc4c6bbfd3640a7bddbe3a0cbf98d
Francisco Franco <reifk14@gmail.com>
2023-08-28 14:57:22 -0300

    change navbar  color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
 	return (
-		<header className='bg-blue-200	 py-5 flex justify-center rounded-2xl shadow-lg shadow-white-400 '>
+		// bg-gradient-to-r from-purple-500 to-pink-500
+		<header className='bg-gradient-to-r from-sky-200 to-white-300	 text-transparent py-5 flex justify-center rounded-2xl shadow-lg shadow-white-400 '>
 			<Link to='/'>
 				<h1 className='text-slate-700 text-3xl font-bold hover:scale-110 transition-all duration-500 '>
 					World Quiz App
